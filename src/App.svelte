48cf9cd8d420c7e605877874d1cf53a2bba2b163
arthurpip <tutucavieira1@gmail.com>
2023-07-04 11:45:31 -0300

    troca da linguagem

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
 	let principal = 0;
-	let rate = 0;
-	let time = 0;
-	let result = 0;
+	let taxa = 0;
+	let tempo = 0;
+	let resultado = 0;
   
-	function calculate() {
-	  const calculatedRate = 1 + rate / 100;
-	  result = principal * Math.pow(calculatedRate, time);
+	function calcular() {
+	  const calculatarTaxa = 1 + taxa / 100;
+	  resultado = principal * Math.pow(calculatarTaxa, tempo);
 	}
   </script>
   
@@ -20,21 +20,21 @@
   
 	<div class="receber">
 	  <label for="rate">Taxa de Juros (%):</label>
-	  <input type="number" id="rate" bind:value="{rate}" />
+	  <input type="number" id="rate" bind:value="{taxa}" />
 	</div>
   
 	<div class="receber">
 	  <label for="time">Tempo (anos):</label>
-	  <input type="number" id="time" bind:value="{time}" />
+	  <input type="number" id="time" bind:value="{tempo}" />
 	</div>
   
-	<button on:click="{calculate}"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+	<button on:click="{calcular}"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
 		<path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
 	  </svg></button>
   
-	{#if result}
+	{#if resultado}
 	  <div class="result">
-		<p>O valor total é: {result.toFixed(2)}</p>
+		<p>O valor total é: {resultado.toFixed(2)}</p>
 	  </div>
 	{/if}
 
